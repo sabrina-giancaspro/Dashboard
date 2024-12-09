38b5b63dca5fbcb7535f912f21e8269b4142eb1a
sabrina-giancaspro <sabrinatwitter6@gmail.com>
2024-12-09 15:49:15 +0100

    deleted comments

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -5,7 +5,6 @@ import { ViewsComponent } from '../pages/dashboard/widgets/views.component';
 
 @Injectable()
 export class DashboardService {
-  // Available widgets
   widgets = signal<Widget[]>([
     {
       id: 1,
@@ -40,17 +39,13 @@ export class DashboardService {
       img: '/views.png'
     },
   ]);
-
-  // Added widgets
   addedWidgets = signal<Widget[]>([]);
 
-  // Widgets that can be added (not already added)
   widgetsToAdd = computed(() => {
     const addedIds = this.addedWidgets().map(w => w.id);
     return this.widgets().filter(w => !addedIds.includes(w.id));
   });
 
-  // Method to add a widget to the dashboard
   addWidgetToDashboard(widget: Widget) {
     this.addedWidgets.update(widgets => [...widgets, widget]);
   }
